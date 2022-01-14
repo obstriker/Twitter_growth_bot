06cf4f3a040ad261bb96e1bc585297fb01d51f9b
itayva <dumsacc@gmail.com>
2022-01-14 03:05:29 +0200

    add comments for future requests

--- twitter_growth_bot.py
+++ twitter_growth_bot.py
@@ -11,7 +11,7 @@ parser.add_argument("--username", help="The username that you want to promote", 
 parser.add_argument("--password", help="The password that you want to promote", type=str)
 parser.add_argument("--follow-list", help="Follow this list as follow/unfollow technique", type=str)
 parser.add_argument("--follow-unfollow", help="use the follow/unfollow technique", type=str)
-parser.add_argument("--domain-hashtag", help="Hashtag for interest discovery", type=str)
+parser.add_argument("--hashtag", help="Hashtag for interest discovery", type=str)
 args = parser.parse_args()
     
 
@@ -21,6 +21,7 @@ def follow_unfollow_technique(username, password, hashtags, \
      max_follow_overall = MAX_OVERALL_FOLLOW):
 
     r = registered_user_manager(username, password)
+    # Check if this hashtag already loaded, then decide wether to load it again.
     interesting_followers = r.load_followers_from_hashtag(hashtags)
     r.follow(interesting_followers)
     r.unfollow_followed_people()
